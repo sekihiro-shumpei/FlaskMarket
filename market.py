f121c854db9436e52396d114a864b0dc2e10fa4a
Shumpei Sekihiro <ssekihiro@outlook.jp>
2024-12-01 13:35:53 +0900

    39分08秒

--- market.py
+++ market.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def hello_world():
-    return "<h1>Hello, World but bigger</h1>"
+    return render_template('home.html')
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>This is the about page of {username}</h1>'
