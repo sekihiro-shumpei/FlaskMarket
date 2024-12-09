876ad15768c50c5e10b899e396b4a76086387cf0
sekihiro-company <sekihiro@seedna.co.jp>
2024-12-09 21:26:32 +0900

    12月9日進捗

--- market.py
+++ market.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>This is the about page of {username}</h1>'
+# @app.route('/about/<username>')
+# def about_page(username):
+#     return f'<h1>This is the about page of {username}</h1>'
+
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html', items=items)
