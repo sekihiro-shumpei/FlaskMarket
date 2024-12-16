77403a792017957f5a4b1a4d2424b17c436871ac
Shumpei Sekihiro <ssekihiro@outlook.jp>
2024-12-16 14:12:57 +0900

    3h13m進捗

--- market/routes.py
+++ market/routes.py
@@ -29,4 +29,8 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user: {err_msg}')
+
     return render_template('register.html', form=form)
